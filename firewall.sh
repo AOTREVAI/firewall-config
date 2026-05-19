47602f912f0f454e629f7fef5bab07e647472d01
fetente <fetente@iurinbox.sbu>
2026-05-19 13:08:56 +0200

    check con prof

--- firewall.sh
+++ firewall.sh
@@ -35,12 +35,14 @@ $IPT -A FORWARD -i enp0s8 -o enp0s3 -j LAN-PUB
 $IPT -A FORWARD -i enp0s3 -o enp0s8 -j PUB-LAN
 $IPT -A FORWARD -i enp0s9 -o enp0s3 -j DMZ-PUB
 $IPT -A FORWARD -i enp0s3 -o enp0s9 -j PUB-DMZ
+
 # SNAT
-$IPT -t nat -A POSTROUTING -o enp0s3 -s 192.168.10.0/24 -j SNAT --to-source 172.22.202.10
+$IPT -t nat -A POSTROUTING -o enp0s3 -s 192.168.14.0/24 -j SNAT --to-source 172.22.202.14
+$IPT -t nat -A POSTROUTING -o enp0s3 -s 10.10.14.0/24 -j SNAT --to-source 172.22.202.14
 
 # INPUT
 # aprire porta per ssh su (i)nterfacci/a (s)ource (p)rotocol portadestinazione
-$IPT -A INPUT -i enp0s3 -s 172.22.202.102 -p tcp --dport 2222 -j ACCEPT  
+$IPT -A INPUT -i enp0s3 -s 172.22.202.102 -p tcp --dport 2222 -j ACCEPT # dipende da indirizzo ip host per ssh 
 
 # apre la porta per connessioni iniziate da noi e ancora attive
 $IPT -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
@@ -51,25 +53,29 @@ $IPT -A INPUT -p icmp --icmp-type echo-request -j ACCEPT
 
 # FORWARD
 # $IPT -A FORWARD -i enp0s8 -o enp0s3 -s 192.168.10.0/24 -p icmp --icmp-type 8 -j ACCEPT
-#
 # $IPT -A FORWARD -i enp0s3 -o enp0s8 -s 192.168.10.0/24 -m state --state ESTABLISHED,RELATED -j ACCEPT
-#
 # $IPT -A FORWARD -i enp0s8 -o enp0s9 -s 192.168.10.0/24 -d 10.10.10.0/24 -p icmp --icmp-type 8 -j ACCEPT
-#
 # $IPT -A FORWARD -i enp0s9 -o enp0s8 -s 192.168.10.0/24 -m state --state ESTABLISHED,RELATED -j ACCEPT
 
 #LAN-PUB
-$IPT -A LAN-PUB -s 192.168.10.0/24 -p icmp --icmp-type 8 -j ACCEPT
-$IPT -A LAN-PUB -s 192.168.10.0/24 -p tcp --dport 80 -j ACCEPT
-$IPT -A LAN-PUB -s 192.168.10.0/24 -p tcp --dport 443 -j ACCEPT
-$IPT -A LAN-PUB -s 192.168.10.0/24 -p udp --dport 53 -j ACCEPT
+$IPT -A LAN-PUB -s 192.168.14.0/24 -p icmp --icmp-type 8 -j ACCEPT
+$IPT -A LAN-PUB -s 192.168.14.0/24 -p tcp --dport 80 -j ACCEPT
+$IPT -A LAN-PUB -s 192.168.14.0/24 -p tcp --dport 443 -j ACCEPT
+$IPT -A LAN-PUB -s 192.168.14.0/24 -p udp --dport 53 -j ACCEPT
 
 #PUB-LAN 
-$IPT -A PUB-LAN -d 192.168.10.0/24 -m state --state ESTABLISHED,RELATED -j ACCEPT
+$IPT -A PUB-LAN -d 192.168.14.0/24 -m state --state ESTABLISHED,RELATED -j ACCEPT
 
 #LAN-DMZ
-$IPT -A LAN-DMZ -s 192.168.10.0/24 -d 10.10.10.0/24 -p icmp --icmp-type 8 -j ACCEPT
+$IPT -A LAN-DMZ -s 192.168.14.0/24 -d 10.10.14.0/24 -p icmp --icmp-type 8 -j ACCEPT
 
 #DMZ-LAN
-$IPT -A DMZ-LAN -s 192.168.10.0/24 -m state --state ESTABLISHED,RELATED -j ACCEPT
+$IPT -A DMZ-LAN -s 192.168.14.0/24 -m state --state ESTABLISHED,RELATED -j ACCEPT
 
+# DMZ-PUB
+$IPT -A DMZ-PUB -s 10.10.14.0/24 -p udp --dport 53 -j ACCEPT
+$IPT -A DMZ-PUB -s 10.10.14.0/24 -p tcp --dport 80 -j ACCEPT
+$IPT -A DMZ-PUB -s 10.10.14.0/24 -p tcp --dport 443 -j ACCEPT
+
+# PUB-DMZ
+$IPT -A PUB-DMZ -d 10.10.14.0/24 -m state --state ESTABLISHED,RELATED -j ACCEPT
